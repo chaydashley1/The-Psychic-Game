0f92985a2ddba08cec2859b35af2dacd273b1429
Ashley Morales <ashley.morales.figueroa@gmail.com>
2019-06-20 01:33:50 -0400

    anadiendo get elementbyID

---
+++ events/main.js
@@ -0,0 +1,31 @@
+var winplayer = document.getElementById("win-player");
+if (1<9);
+document.write("you win: "+ "sera esto?");
+
+var loseplayer = document.getElementById("lose-player");
+if (1<9);
+document.write("you lose: "+ "ESTO?");
+
+var guessesLeft = document.getElementById("guesses-left");
+GuessesLeft = 4;
+if (1<9);
+document.write("Guesses Left: "+ "???");
+
+
+var textselect = document.getElementById("text-select");
+
+
+
+
+
+
+
+
+      // for var i=0; i => lenght.keyPlayer; i ++);
+    
+
+      document.onkeyup = function(event) {
+        textselect.textContent = event.key;
+      };
+
+     
